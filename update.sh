7684c019091779fb29aeb629cfc20c3dcaedfcee
Veivneorul <thelightguardian@outlook.fr>
2024-06-10 15:03:05 +0200

    Updated update.sh

--- update.sh
+++ update.sh
@@ -8,6 +8,8 @@ if [ $EUID -ne 0 ]; then
     else echo "Vous avez les permissions administrateurs"
 fi
 
+echo "Début de l'opération de mise à niveau de netbox"
+
 # Vérifie les mises à jours et les installes
 echo "Recherche et mise à jour du système et des paquets"
 apt update && apt upgrade -y
@@ -48,8 +50,19 @@ else
   echo "Le fichier $fichier_source n'existe pas."
 fi
 
+fichier_source="/opt/netbox-$OLDVER/netbox/netbox/ldap_config.py"
+fichier_destination="/opt/netbox/netbox/netbox/ldap_config.py"
+
+if [ -f "$fichier_source" ]; then
+  # Le fichier existe, on le copie
+  cp "$fichier_source" "$fichier_destination"
+  echo "Fichier copié avec succès !"
+else
+  # Le fichier n'existe pas, on affiche un message
+  echo "Le fichier $fichier_source n'existe pas."
+fi
+
 cp /opt/netbox-$OLDVER/netbox/netbox/configuration.py /opt/netbox/netbox/netbox/
-cp /opt/netbox-$OLDVER/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/
 cp -pr /opt/netbox-$OLDVER/netbox/media/ /opt/netbox/netbox/
 cp -r /opt/netbox-$OLDVER/netbox/scripts /opt/netbox/netbox/
 cp -r /opt/netbox-$OLDVER/netbox/reports /opt/netbox/netbox/
@@ -61,3 +74,9 @@ cd /opt/netbox/
 systemctl restart netbox netbox-rq
 
 ln -s /opt/netbox/contrib/netbox-housekeeping.sh /etc/cron.daily/netbox-housekeeping
+
+echo "Suppression de l'ancienne version..."
+
+rm -R /opt/netbox-$OLDVER/
+
+echo "La mise à niveau est terminé !"
